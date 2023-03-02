302142b204785f844513aa9dba1c7c5f1bd5a810
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-03-02 06:04:45 +0000

    aeg

---
+++ ES6_basic/9-getFullBudget.js
@@ -0,0 +1,22 @@
+mport getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
